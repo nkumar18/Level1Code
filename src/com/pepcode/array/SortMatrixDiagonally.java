d7b8b857df183d6feb10573b01f9b32b00f8fb34
niranjan <niranjankrssec@gmail.com>
2022-08-13 21:33:13 +0800

    https://leetcode.com/problems/sort-the-matrix-diagonally/submissions/

--- src/com/pepcode/array/SortMatrixDiagonally.java
+++ src/com/pepcode/array/SortMatrixDiagonally.java
@@ -3,6 +3,8 @@ package com.pepcode.array;
 Time complexity mn (log(min(m,n))
 diagonal ki length m or n m jo chota hota h
  */
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
@@ -32,5 +34,33 @@ public class SortMatrixDiagonally {
             }
         }
      return mat;
+    }
+    public int[][] diagonalSort_2ndApproch(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for(int gap = -(m - 1); gap <= (n - 1); gap++){
+            ArrayList<Integer> diag = new ArrayList<>();
+            int i = gap < 0? -gap: 0;
+            int j = gap < 0? 0: gap;
+            while(i < m && j < n){
+                diag.add(mat[i][j]);
+                i++;
+                j++;
+            }
+
+            Collections.sort(diag);
+            i = gap < 0? -gap: 0;
+            j = gap < 0? 0: gap;
+            int idx = 0;
+            while(i < m && j < n){
+                mat[i][j] = diag.get(idx);
+                idx++;
+                i++;
+                j++;
+            }
+        }
+
+        return mat;
     }
 }
